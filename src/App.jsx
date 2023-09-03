9726fdd63439fac4742aefb3e4cff39aab6433ed
K Harsh <harshnaidu2u@gmail.com>
2023-09-03 15:34:35 +0530

    delete logic INTEGER Fromatter

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,21 @@ function reducer(state, {type, payload}){
         }
     case ACTIONS.CLEAR:
         return {}
+    case ACTIONS.DELETE_DIGIT:
+      if(state.overWrite){
+        return{
+          ...state,
+          overWrite: false,
+          currentOperand: null
+        }
+      }
+      if(state.currentOperand == null ){ return state}
+      if(state.currentOperand.length == 1 ){ return {...state, currentOperand: null}
+       }
+       return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+       }
     case ACTIONS.EVALUATE:
           if(
             state.operation == null || 
@@ -98,6 +113,16 @@ function evaluate({currentOperand, previousOperand, operation}){
   return computation.toString();
 }
 
+const INTEGER_FROMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0, 
+})
+function formatOperand(operand){
+  if(operand == null) return;
+  const [integer, decimal] = operand.split(".")
+  if(decimal == null) return INTEGER_FROMATTER.format(integer)
+  return `${INTEGER_FROMATTER.format(integer)}.${decimal}`
+}
+
 function App() {
 const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
 
@@ -105,13 +130,13 @@ const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(redu
     <>
       <div className="calculator-grid">
         <div className="output">
-          <div className="previous-operand">{previousOperand} {operation} </div>
-          <div className="current-operand"> {currentOperand} </div>
+          <div className="previous-operand">{formatOperand(previousOperand)} {operation} </div>
+          <div className="current-operand"> {formatOperand(currentOperand)} </div>
         </div>
 
         <button className="span-two" 
         onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
-        <button>DEL</button>
+        <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
         <DigitButton digit="÷" dispatch={dispatch}/>
         <DigitButton digit="1" dispatch={dispatch}/>
         <DigitButton digit="2" dispatch={dispatch}/>
